57571da98a01cb9e12a0d75d2c9799ad420ec7e8
Maxim Kanashin <maxkanashin@gmail.com>
2021-01-19 18:03:42 +0300

    Working with User сomponent and styles

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Avatar } from '@material-ui/core'
+import './UserListItem.css'
 
 function UserListItem() {
   return (
     <div className="userListItem">
-      <h1>User</h1>
+      <div className="userListItem--avatar">
+        <Avatar />
+      </div>
+      <div className="userListItem--container">
+        <div className="userListItem--container--line">
+          <div className="userListItem--user">User Name</div>
+          <div className="userListItem--data">00:00</div>
+        </div>
+        <div className="userListItem--container--line">
+          <div className="userListItem--message">Hello world!</div>
+        </div>
+      </div>
     </div>
   )
 }
