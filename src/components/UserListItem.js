1b186cef768b64aa0b390d6294f822cd33d86c3d
Maxim Kanashin <maxkanashin@gmail.com>
2021-01-20 15:31:38 +0300

    fixed output of long messages

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -14,7 +14,14 @@ function UserListItem() {
           <div className="userListItem--data">00:00</div>
         </div>
         <div className="userListItem--container--line">
-          <div className="userListItem--message">Hello world!</div>
+          <div className="userListItem--message">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
+              ipsam, porro voluptas necessitatibus omnis libero nam vel
+              laudantium autem modi! Pariatur quasi error repudiandae saepe
+              necessitatibus fuga, dolorum ratione quis.
+            </p>
+          </div>
         </div>
       </div>
     </div>
