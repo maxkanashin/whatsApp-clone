d32c7e5cf87b5c19c64b10ab28fd9cfade7c77fc
Maxim Kanashin <maxkanashin@gmail.com>
2021-01-26 20:51:29 +0300

    fix MessageBox

--- src/components/shared/MessageBox.js
+++ src/components/shared/MessageBox.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './MessageBox.css'
+import { MY_UUID } from '../../constants'
 
 function MessageBox({ message }) {
   return (
     <div className="messageBox--line">
-      <div className="messageBox--line--container">
+      <div
+        className={`messageBox--line--container${
+          message.author === MY_UUID ? ` right` : ``
+        }`}
+      >
         <div className="container--message">{message.message}</div>
         <div className="container--time">{message.date}</div>
       </div>
