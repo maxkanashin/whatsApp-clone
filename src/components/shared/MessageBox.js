c5c566e82a04cccec594b1996e1ac0273e8c3a5e
Maxim Kanashin <maxkanashin@gmail.com>
2021-01-28 20:59:17 +0300

    add button to MessageBox

--- src/components/shared/MessageBox.js
+++ src/components/shared/MessageBox.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './MessageBox.css'
+import { IconButton } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { MY_UUID } from '../../constants'
 
 function MessageBox({ message }) {
@@ -7,11 +9,18 @@ function MessageBox({ message }) {
     <div className="messageBox--line">
       <div
         className={`messageBox--line--container${
-          message.author === MY_UUID ? ` right` : ``
+          message.author === MY_UUID ? ` out` : ``
         }`}
       >
-        <div className="container--message">{message.message}</div>
-        <div className="container--time">{message.date}</div>
+        <div className="container--wrapper">
+          <div className="container--wrapper--message">{message.message}</div>
+          <div className="container--wrapper--time">{message.date}</div>
+        </div>
+        <div className="container--btn">
+          <IconButton>
+            <ExpandMoreIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   )
